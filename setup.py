b5a83baa751ad1517e45f15ec6f7bec822686f02
flying-anime <155505091+flying-anime@users.noreply.github.com>
2025-03-17 20:16:14 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="Generative AI Project",
+    version="0.0.0",
+    author="Akshaya",
+    email="kumarakshaya9440@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
